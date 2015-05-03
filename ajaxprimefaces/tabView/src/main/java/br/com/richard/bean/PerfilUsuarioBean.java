7ecd412ef82217f3f6cda3fb4fb25de05bb6a7e6
Ricardolv <richardluizv@gmail.com>
2015-05-03 14:49:18 -0300

    commit

---
+++ ajaxprimefaces/tabView/src/main/java/br/com/richard/bean/PerfilUsuarioBean.java
@@ -0,0 +1,21 @@
+package br.com.richard.bean;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+
+@ManagedBean
+@ViewScoped
+public class PerfilUsuarioBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	
+	public Date getDataAtual() {
+		return new Date();
+	}
+	
+}
